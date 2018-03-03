cefef47300c9ad44d16bcd5c43948989d58c0814
Ola Olsen <31655537+olaolsen22@users.noreply.github.com>
2018-03-03 23:36:57 +0800

    That's it!

---
+++ NumberTextField.java
@@ -0,0 +1,19 @@
+package sample.validateFields;
+
+import javafx.scene.control.TextField;
+
+public class NumberTextField extends TextField {
+    public NumberTextField() {
+    }
+
+    public void replaceText(int i, int i1, String string) {
+        if ((this.getText() + string).matches("(\\d*(?:\\.\\d*)?)")) {
+            super.replaceText(i, i1, string);
+        }
+
+    }
+
+    public void replaceSelection(String string) {
+        super.replaceSelection(string);
+    }
+}
